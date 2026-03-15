41801ca1e19151e121b345eb6d62ca2b529c256a
yuriiT09 <tvyuchuktv@gmail.com>
2026-03-15 08:01:46 +0000

    Add main menue for file finder

--- src/FileFinder/FileFinder/Program.cs
+++ src/FileFinder/FileFinder/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        while (true)
+        {
+            Console.WriteLine("File Finder");
+            Console.WriteLine("1. Search files by name");
+            Console.WriteLine("2. Search text inside files");
+            Console.WriteLine("3. Exit");
+            Console.Write("Choose an option: ");
+
+            string choice = Console.ReadLine();
+            Console.WriteLine();
+
+            if (choice == "1")
+            {
+                Console.WriteLine("File search will be added here.\n");
+            }
+            else if (choice == "2")
+            {
+                Console.WriteLine("Text search will be added here.\n");
+            }
+            else if (choice == "3")
+            {
+                Console.WriteLine("Program finished.");
+                break;
+            }
+            else
+            {
+                Console.WriteLine("Invalid option.\n");
+            }
+        }
+    }
+}
